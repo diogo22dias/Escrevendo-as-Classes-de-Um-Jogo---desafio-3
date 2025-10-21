f814e7795614c33b350d4b54349256251ae64818
diogo22dias <diogo22diasdev@gmail.com>
2025-10-21 18:58:50 -0300

    finalizei

--- desafio3.js
+++ desafio3.js
@@ -1,7 +1,39 @@
 
-    class heroi {
-        constructor(nome, idade, tipo)
+   class heroi {
+        constructor(nome, idade, tipo) {
 this.nome = nome;
 this.idade = idade;
 this.tipo = tipo;
-    }
+    
+}
+    atacar() {
+        let ataque;
+            if (this.tipo === "mago") {
+                ataque = "usou magia";
+            } else if (this.tipo === "guerreiro") {
+                ataque = "usou espada";
+            } else if (this.tipo === "monge") {
+                ataque = " usou artes marciais";
+            } else if (this.tipo === "ninja") {
+                ataque = "usou shuriken";
+            } else {
+                ataque = "usou um ataque secreto";
+            }
+            
+            
+            console.log( `o ${this.tipo} atacou usando ${ataque}`);
+       
+        }
+        } 
+
+        const heroi1 = new heroi("Malandraquias",180, "mago");
+        const heroi2 = new heroi("leonidas", 53, "guerreiro")
+        const heroi3 = new heroi("jackie chan", 30, "monge")
+        const heroi4 = new heroi("Naruto", 25, "ninja")
+        const heroi5 = new heroi("ligerius", 23, "Druida")
+
+        heroi1.atacar();
+		heroi2.atacar();
+		heroi3.atacar();
+		heroi4.atacar();
+		heroi5.atacar();
